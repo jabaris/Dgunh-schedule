2d91831ffc878fc60d463cc70e71fe64111d9698
jabaris <130151127+jabaris@users.noreply.github.com>
2024-08-12 21:05:55 +0300

    Чередование дня недели починил

--- get_schedule.php
+++ get_schedule.php
@@ -14,14 +14,11 @@ if ($conn->connect_error) {
 
 $userType = $_POST['userType'];
 
-// Определяем текущий день недели (1 - понедельник, ..., 7 - воскресенье)
-// $currentWeekday = date('N') - 3; // N возвращает день недели, начиная с 1 для понедельника до 7 для воскресенья.
-
 
 // Получаем параметры из POST-запроса
 $userType = $_POST['userType'];
 $requestedDate = new DateTime($_POST['date']);  // Переданная дата
-$currentWeekday = $_POST['weekday']; 
+$currentWeekday = $_POST['weekday'];
 
 // Получаем начало и конец семестра из базы данных
 $sql_semester = "SELECT date_start, date_end FROM schedule LIMIT 1"; // предположим, что даты одинаковы для всех записей
@@ -37,12 +34,14 @@ if ($result_semester->num_rows > 0) {
 
 // Рассчитываем текущую неделю семестра
 $today = new DateTime();
-$weekDifference = $dateStart->diff($today)->days / 7;
+$weekDifference = $dateStart->diff($requestedDate)->days / 7;
 $currentWeek = ceil($weekDifference);
 
 // Определяем, какая сейчас неделя (1 или 2)
 $weekType = $currentWeek % 2 == 0 ? 2 : 1;
+
 error_log($weekType);
+
 if ($userType == "student") {
     $faculty = $_POST['faculty'];
     $studyForm = $_POST['studyForm'];
@@ -65,6 +64,7 @@ if ($userType == "student") {
             INNER JOIN schedule_ring AS sr ON sr.education_form_id = ef.id AND sr.lesson_num = s.lesson_num
             INNER JOIN lesson_type AS lt ON s.lesson_type = lt.id
             INNER JOIN faculty AS f ON epr.faculty_id = f.id
+            INNER JOIN schedule_week AS sw ON sw.education_form_id = ef.id 
             WHERE f.faculty_name = ? AND ef.education_form_name = ? AND el.education_level_name = ? AND epr.education_profile_name = ? AND ep.education_program_name = ?
             AND eg.num = ? AND (eg.subnum = ? OR eg.subnum IS NULL) AND s.weekday = ? AND s.week = ?
             ORDER BY s.lesson_num";
